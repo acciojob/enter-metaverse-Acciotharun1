33405fdf2cabf9f17a8a3851769f95b2c28ac005
Acciotharun1 <166792506+Acciotharun1@users.noreply.github.com>
2024-10-07 09:26:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let ptext=document.getElementById("status");
+let button=document.getElementById("enterBtn");
+button.addEventListener("click", function(){
+	let h1=document.createElement("h1");
+	h1.textContent="Entered Metaverse";
+	ptext.replaceWith(h1);
+})
